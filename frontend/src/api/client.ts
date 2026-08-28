6f778c629e938db998a099f7c69a6764773032e1
Shahir1134 <shahir55312@gmail.com>
2026-08-28 22:47:11 +0530

    added fixed

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://scam-detection-razorpay-hackathon.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
